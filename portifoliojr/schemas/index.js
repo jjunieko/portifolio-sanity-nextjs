bc992be082a6437457a388c2979815c188e97b02
joao.ribeiro <joao.ribeiro@brbcard.com.br>
2023-02-02 00:14:49 -0300

    first commit

---
+++ portifoliojr/schemas/index.js
@@ -0,0 +1,18 @@
+
+import header from './header';
+import about from './about';
+import social from './social';
+import contact from './contact';
+import projects from './projects';
+import skills from './skills';
+import footer from './footer';
+
+export const schemaTypes = [
+    header,
+    about,
+    social,
+    contact,
+    projects,
+    skills,
+    footer
+]
